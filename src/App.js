2f96a59105289527e8da1860a8f56af83ba13819
Satyadev-Patel <satyadev300302@gmail.com>
2021-06-11 02:49:11 +0530

    task updated

--- src/App.js
+++ src/App.js
@@ -23,17 +23,30 @@ function App() {
         reminder : false,
     },
   ])
-
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter(
       (task) => task.id !== id
     ))
   }
 
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { 
+      ...task,
+      reminder: !task.reminder,
+     } : task))
+  }
+
   return (
     <div className="container">
       <Header title='Hello' />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}
+        onToggle={toggleReminder}/>
+      ) : (
+        'No Tasks'
+      )}
     </div>
   );
 }
